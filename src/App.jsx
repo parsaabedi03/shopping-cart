8b82662762c0e5b51bc733f438d0b22741730f2a
Parsa Abedi <learnforwork2022@gmail.com>
2026-02-17 20:48:26 +0330

    Wrapping Layouts around App

--- src/App.jsx
+++ src/App.jsx
@@ -3,16 +3,19 @@ import ProductsPage from "./pages/ProductsPage";
 import ProductDetailsPage from "./pages/ProductDetailsPage";
 import CheckoutPage from "./pages/CheckoutPage";
 import PageNotFound from "./pages/404page";
+import Layouts from "./layouts/Layouts";
 
 function App() {
   return (
-    <Routes>
-      <Route index element={<Navigate to="/products" replace />} />
-      <Route path="/products" element={<ProductsPage />} />
-      <Route path="/products/:id" element={<ProductDetailsPage />} />
-      <Route path="/checkout" element={<CheckoutPage />} />
-      <Route path="*" element={<PageNotFound />} />
-    </Routes>
+    <Layouts>
+      <Routes>
+        <Route index element={<Navigate to="/products" replace />} />
+        <Route path="/products" element={<ProductsPage />} />
+        <Route path="/products/:id" element={<ProductDetailsPage />} />
+        <Route path="/checkout" element={<CheckoutPage />} />
+        <Route path="*" element={<PageNotFound />} />
+      </Routes>
+    </Layouts>
   );
 }
 
