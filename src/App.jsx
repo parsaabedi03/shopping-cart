da0191face5d0615da312212ff57f4858f6de47b
Parsa Abedi <learnforwork2022@gmail.com>
2026-02-17 19:58:26 +0330

    Adding routes to App

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,19 @@
+import { Navigate, Route, Routes } from "react-router";
+import ProductsPage from "./pages/ProductsPage";
+import ProductDetailsPage from "./pages/ProductDetailsPage";
+import CheckoutPage from "./pages/CheckoutPage";
+import PageNotFound from "./pages/404page";
+
 function App() {
-  return <h1>shopping cart</h1>;
+  return (
+    <Routes>
+      <Route index element={<Navigate to="/products" replace />} />
+      <Route path="/products" element={<ProductsPage />} />
+      <Route path="/products/:id" element={<ProductDetailsPage />} />
+      <Route path="/checkout" element={<CheckoutPage />} />
+      <Route path="*" element={<PageNotFound />} />
+    </Routes>
+  );
 }
 
 export default App;
