c3cdd67577dbb3433ad94bee42980b96d1c0df09
Parsa Abedi <learnforwork2022@gmail.com>
2026-02-18 23:22:14 +0330

    wrap App with AppProviders

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,14 @@ import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router";
 import App from "./App.jsx";
 import "./global.css";
+import AppProviders from "./providers/AppProviders.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter>
-      <App />
+      <AppProviders>
+        <App />
+      </AppProviders>
     </BrowserRouter>
   </StrictMode>,
 );
