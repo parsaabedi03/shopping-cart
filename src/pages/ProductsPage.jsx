0e0d15c56641b4a04ef30e92d5617c1e66f84309
Parsa Abedi <learnforwork2022@gmail.com>
2026-02-19 23:23:42 +0330

    implement page layout and add Card component

--- src/pages/ProductsPage.jsx
+++ src/pages/ProductsPage.jsx
@@ -1,13 +1,28 @@
 import SearchBar from "../components/SearchBar";
 import useProducts from "../context/products/useProducts";
+import Card from "../components/Card";
+
+import styles from "./ProductsPage.module.css";
 
 function ProductsPage() {
   const products = useProducts();
-  console.log(products);
   return (
-    <div>
-      <SearchBar />
-    </div>
+    <>
+      <div>
+        <SearchBar />
+      </div>
+      <div className={styles.container}>
+        <div className={styles.productContainer}>
+          {!products.length && <p>is Loading ...</p>}
+          {products.map((product) => (
+            <Card key={product.id} data={product} />
+          ))}
+        </div>
+        <div className={styles.category}>
+          <aside><p>this is category</p></aside>
+        </div>
+      </div>
+    </>
   );
 }
 
