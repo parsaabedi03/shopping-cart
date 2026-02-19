f332f1956318cb70865fd1bd619e2a0f488152da
Parsa Abedi <learnforwork2022@gmail.com>
2026-02-19 23:29:41 +0330

    render title using useParams to verify functionality

--- src/pages/ProductDetailsPage.jsx
+++ src/pages/ProductDetailsPage.jsx
@@ -1,7 +1,26 @@
+import { useEffect, useState } from "react";
+import { useParams } from "react-router";
+import api from "../services/config";
+
 function ProductDetailsPage() {
+  const [productDetails, setProductDetails] = useState([]);
+  const { id } = useParams();
+
+  useEffect(() => {
+    const getProductDetails = async () => {
+      try {
+        setProductDetails(await api.get(`/products/${id}`));
+      } catch (error) {
+        console.log(error.message);
+      }
+    };
+    getProductDetails();
+  }, [id]);
   return (
-    <div>ProductDetailsPage</div>
-  )
+    <div>
+      <h3>{productDetails.title}</h3>
+    </div>
+  );
 }
 
-export default ProductDetailsPage
+export default ProductDetailsPage;
