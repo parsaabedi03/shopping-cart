555629ea4ff82ccd8722885efe1fad8158d2b32c
Parsa Abedi <parsaab03@gmail.com>
2026-02-24 21:04:34 +0330

    Wrap App with CartProvider

--- src/providers/AppProviders.jsx
+++ src/providers/AppProviders.jsx
@@ -1,7 +1,12 @@
+import CartProvider from "../context/cart/CartProvider";
 import ProductsProvider from "../context/products/ProductsProvider";
 
 function AppProviders({ children }) {
-  return <ProductsProvider>{children}</ProductsProvider>;
+  return (
+    <ProductsProvider>
+      <CartProvider>{children}</CartProvider>
+    </ProductsProvider>
+  );
 }
 
 export default AppProviders;
