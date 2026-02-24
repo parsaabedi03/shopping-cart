21b25b99cb7d7cd549f6e0e60b24d70c1c31edcc
Parsa Abedi <parsaab03@gmail.com>
2026-02-24 21:40:24 +0330

    integrate useCart into Card component

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -4,9 +4,13 @@ import { Link } from "react-router";
 import { shortenText } from "../helper/helper";
 
 import styles from "./Card.module.css";
+import useCart from "../context/cart/useCart";
 
 function Card({ data }) {
   const { id, image, title, price } = data;
+  const { state, dispatch } = useCart();
+
+  const getQuantity = state.data.find((item) => item.id === id);
 
   return (
     <div className={styles.card}>
@@ -20,16 +24,31 @@ function Card({ data }) {
           <LayoutList className={styles.prodcutDetails} />
         </Link>
         <div className={styles.buttons}>
-          <button style={{ display: "none" }}>
+          <button
+            className={getQuantity?.quantity >= 2 ? "" : styles.dNone}
+            onClick={() => dispatch({ type: "DECREASE", payload: id })}
+          >
             <Minus className={styles.icons} />
           </button>
-          <button style={{ display: "none" }}>
-            <Plus className={styles.icons} />
-          </button>
-          <button style={{ display: "none" }}>
+          <button
+            className={getQuantity?.quantity !== 1 ? styles.dNone : ""}
+            onClick={() => dispatch({ type: "REMOVE", payload: id })}
+          >
             <Trash2 className={styles.icons} />
           </button>
-          <button>
+
+          {getQuantity?.quantity >= 1 && <span>{getQuantity.quantity}</span>}
+
+          <button
+            className={getQuantity?.quantity >= 1 ? "" : styles.dNone}
+            onClick={() => dispatch({ type: "INCREASE", payload: id })}
+          >
+            <Plus className={styles.icons} />
+          </button>
+          <button
+            className={getQuantity?.quantity >= 1 ? styles.dNone : ""}
+            onClick={() => dispatch({ type: "ADD", payload: data })}
+          >
             <Handbag className={styles.icons} />
           </button>
         </div>
