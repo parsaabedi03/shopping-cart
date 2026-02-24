04cfc3aae6fe4e5c02c8d36a5427e0026c7ea421
Parsa Abedi <parsaab03@gmail.com>
2026-02-24 21:13:25 +0330

    add navigation link to store name using React Router

--- src/layouts/Header.jsx
+++ src/layouts/Header.jsx
@@ -6,7 +6,9 @@ import styles from "./Header.module.css";
 function Header() {
   return (
     <header className={styles.header}>
-      <p>Shopping website</p>
+      <Link to="/products">
+        Shopping website
+      </Link>
       <Link to="/checkout">
         <div className={styles.linkButton}>
           <ShoppingCart color="#f97316" size={20} />
