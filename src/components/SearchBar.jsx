767bf485631bbe874e1e45e514a8366c1940b18c
Parsa Abedi <parsaab03@gmail.com>
2026-02-25 23:53:28 +0330

    add search functionality

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -2,12 +2,16 @@ import { Search } from "lucide-react";
 
 import styles from "./SearchBar.module.css";
 
-function SearchBar() {
-  const handleSearch = () => {};
-
+function SearchBar({ search, setSearch, handleSearch }) {
   return (
     <div className={styles.searchBarContainer}>
-      <input type="text" placeholder="Search..." name="" />
+      <input
+        type="text"
+        placeholder="Search..."
+        name="search"
+        value={search}
+        onChange={(e) => setSearch(e.target.value)}
+      />
       <button onClick={handleSearch}>
         <Search className={styles.icon} />
       </button>
